24485bd07636002aa3fee206b961cb6d8dd52423
Plimeor <plimeor@gmail.com>
2023-02-16 09:05:16 +0800

    chore: create rush-prettier autoinstaller

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+// Documentation for this file: https://prettier.io/en/configuration.html
+module.exports = {
+  printWidth: 120,
+  endOfLine: 'auto',
+  singleQuote: true,
+  arrowParens: 'avoid',
+  trailingComma: 'none',
+  semi: false
+}
